4baa40615184a9161b0632b675b01dc027fdbf5d
Shobhit Saini <sainishobhit24@gmail.com>
2021-12-06 14:15:49 +0530

    second commit

--- src/models/internModel.js
+++ src/models/internModel.js
@@ -12,19 +12,17 @@ const internSchema = new mongoose.Schema({
         lowercase: true,
         unique: true,
         required: 'Email address is required',
-        validate: { validator: validator.validateEmail, message: 'Please fill a valid email address', isAsync: false },
-        match: [validator.emailregex, 'Please fill a valid email address']
+        validate: { validator: validator.validateEmail, message: 'Please fill a valid email address' },
     },
     mobile: {
         type: Number,
         unique: true,
-        required: 'Mobile Noo. is required',
+        required: 'Mobile No. is required',
         validate: { validator: validator.validateMobile, message: 'Please fill a valid email address', isAsync: false },
-        match: [validator.mobileregex, 'Please fill a valid email address']
     },
     collegeId: {
         type: mongoose.Types.ObjectId,
-        ref: ' College'
+        ref: 'College'
     },
     isDeleted: {
         type: Boolean,
